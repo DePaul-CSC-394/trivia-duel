169dc01368ac5a9a2b4154d26669493144d7dad0
beetsbymango <dannyp1942@gmail.com>
2024-10-12 00:37:02 -0500

    Updated Schema for Game Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_12_050331) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_12_052328) do
+  create_table "games", force: :cascade do |t|
+    t.boolean "complete", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "score", default: 0
