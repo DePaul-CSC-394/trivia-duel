3f00b24adae8c824d338753fa4708ade718f060f
Carlota Arzua Alonso <carlotaarzua@MacBook-Pro.local>
2024-10-16 00:20:54 -0500

    Buzzer logig

--- app/javascript/buzzer.js
+++ app/javascript/buzzer.js
@@ -1,24 +1,20 @@
 let buffer = null;
-let isAnswered = false;
 
 document.addEventListener('keydown', function(event) {
     console.log('Key pressed: ' + event.key);
 
-    if (!isAnswered) {
-        if (buffer === null) {
-            let result = '';
-            if (event.key === 'a' || event.key === 'A') {
-                buffer = 'Player 1';
-                result = 'Player 1 buzzed in!';
-            } else if (event.key === 'l' || event.key === 'L') {
-                buffer = 'Player 2';
-                result = 'Player 2 buzzed in!';
-            }
+    if (buffer === null) {
+        let result = '';
+        if (event.key === 'a' || event.key === 'A') {
+            buffer = 'Player 1';
+            result = 'Player 1 buzzed in!';
+        } else if (event.key === 'l' || event.key === 'L') {
+            buffer = 'Player 2';
+            result = 'Player 2 buzzed in!';
+        }
 
-            if (result) {
-                document.getElementById('result').textContent = result;
-            }
-
+        if (result) {
+            document.getElementById('result').textContent = result;
         }
     }
 });
