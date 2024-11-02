bff52b150a2fd6cc65af1d0cd17ce10eab42587f
Carlota Arzua Alonso <carlotaarzua@MacBook-Pro.local>
2024-11-02 11:32:57 -0500

    Countdown page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def countdown
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get "settings/index"
   root "pages#home"
-  get "pages/home"
+  get "pages/home", to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,8 +10,9 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  get "questions/next", to: "questions#next"
   get "settings", to: "settings#index"
+  get 'countdown', to: 'pages#countdown'
+  get "questions/next", to: "questions#next"
 
   resources :games, only: [ :show ]
 
