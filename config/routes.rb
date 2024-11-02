a35689c3c46312967ffaa08576a6faedae8b1534
Carlota Arzua Alonso <carlotaarzua@MacBook-Pro.local>
2024-11-02 11:38:29 -0500

    Resolved issues

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "settings", to: "settings#index"
-  get 'countdown', to: 'pages#countdown'
+  get "countdown", to: "pages#countdown"
   get "questions/next", to: "questions#next"
 
   resources :games, only: [ :show ]
