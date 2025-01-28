0b0bc0585749c95efd11c654df49acc5cd5f532f
Lily Kerr <lilykerr@Lilys-MacBook-Pro-4.local>
2025-01-27 19:21:14 -0600

    precompile pictures

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile += %w[ *.mp3 ]
 Rails.application.config.assets.precompile += %w[ arcade-countdown.mp3 ]
 Rails.application.config.assets.precompile += %w[ cheering.mp3 ]
+Rails.application.config.assets.precompile += %w[ *.png ]
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
